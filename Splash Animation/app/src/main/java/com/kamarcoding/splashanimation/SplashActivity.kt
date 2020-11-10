4d28915b5e9eb23f29cec6a4d100ecc95edf177c
makhalibagas <makhalibagas1@gmail.com>
2020-11-10 12:22:36 +0700

    splash screen animation

---
+++ Splash Animation/app/src/main/java/com/kamarcoding/splashanimation/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.kamarcoding.splashanimation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash)
+
+
+        Handler().postDelayed({
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }, 10000L) //selama 10 detik splash animation
+    }
+}
